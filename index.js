b4d4170bf3af25b37b55e4c5c14b7654181cae73
Banna Teklebrhan <bannatekle@gmail.com>
2018-07-19 11:19:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById("main").remove();
 
-const newHeader = document.createElement("h1");
- newHeader.setAttribute("id", "victory");
- document.body.appendChild(newHeader);
- newHeader.innerHTML = "is the champion";
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+document.body.appendChild(newHeader);
+newHeader.innerText = "(banna) is the champion";
